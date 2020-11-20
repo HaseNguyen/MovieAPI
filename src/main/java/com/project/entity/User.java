3abe19fd716ef31b80078d729db942704ec588ce
HaseNguyen <46945964+HaseNguyen@users.noreply.github.com>
2020-11-20 18:40:26 +0700

    First upload project

---
+++ src/main/java/com/project/entity/User.java
@@ -0,0 +1,32 @@
+package com.project.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue
+	private int USER_ID;
+	private String MAUSER;
+	@NotBlank(message = "Vui lòng nhập UserName!")
+	private String USERNAME;
+	@NotBlank(message = "Vui lòng nhập mật khẩu!")
+	private String PASSWORD;
+	private String SDT;
+	private String HOTEN;
+	private String AVATAR;
+	
+	@Email(message = "Email ko đúng định dạng!")
+	private String EMAIL;
+	
+	private int GIOITINH;
+}
